eac80514e027eaeedd88cb730545c4e3926bbe57
Mbarek Matis <mbarekmatis@gmail.com>
2025-12-09 11:06:32 +0100

    ajout-stock : REFACTOR

--- src/Laboratory.ts
+++ src/Laboratory.ts
@@ -14,18 +14,12 @@ export class Laboratory {
         return this.stock.get(substance) || 0;
     }
 
-    add(name: string, qty: number): void {
-        if (qty < 0) {
+    add(substance: string, quantity: number): void {
+        if (quantity < 0) {
             throw new Error('Quantity must be positive');
         }
-
-        if (!this.stock.has(name)) {
-            throw new Error('Unknown substance');
-        }
-
-        let old = this.stock.get(name);
-        if (old === undefined) old = 0;
         
-        this.stock.set(name, old + qty);
+        const currentAmount = this.getQuantity(substance);
+        this.stock.set(substance, currentAmount + quantity);
     }
 }
